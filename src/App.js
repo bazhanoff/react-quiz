01bb522d4b1323e356115ee9152c147ae2dc2ad1
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2018-12-03 00:46:09 +0300

    Creates base component of Layout, Layout.css for our project.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <div style={{width: 400, border: '1px solid black'}}>
+                    <h1>Layout works</h1>
+                </div>
+            </Layout>
+        );
+    }
 }
 
 export default App;
