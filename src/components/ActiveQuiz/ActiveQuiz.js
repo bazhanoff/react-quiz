f8b0b610d96a370851e9ea5fef379390da3edead
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2018-12-03 15:19:35 +0300

    Creates the main page

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const ActiveQuiz = props => {
+    return (
+        <div>
+            ActiveQuiz
+        </div>
+    )
+};
+
+export default ActiveQuiz;
