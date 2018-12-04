1a4a86d1d5fd738f6ac2e85b3f307ee2ec409801
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2018-12-04 09:01:14 +0300

    Create containers and components for quiz

--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import classes from './AnswersList.css';
+import AnswerItem from './AnswerItem/AnswerItem';
 
 const AnswersList = props => {
     return (
         <ul className={classes.AnswersList}>
-            {props.answers.map((answer, index) => {
-                    <li>answer</li>
-                }
+            {props.answers.map((answer, index) => (
+                    <AnswerItem
+                        key={index}
+                        answer={answer}
+                    />
+                )
             )}
         </ul>
     )
