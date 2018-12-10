6e3582dab82df8d4c8702f8244f71010471d5a1f
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2018-12-10 17:11:25 +0300

    Adds 3 containers as pages and navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout.js';
-import Quiz from './containers/Quiz/Quiz.js'
+import Quiz from './containers/Quiz/Quiz.js';
+import QuizList from './containers/QuizList/QuizList.js';
+import Auth from './containers/Auth/Auth.js';
+import QuizCreator from './containers/QuizCreator/QuizCreator.js';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
             <Layout>
-                <Quiz  />
+                <Switch>
+                    <Route path='/auth' component={Auth} />
+                    <Route path='/quiz-creator' component={QuizCreator} />
+                    <Route path='/quiz/:id' component={Quiz}/>
+                    <Route path='/' component={QuizList}/>
+                </Switch>
             </Layout>
         );
     }
