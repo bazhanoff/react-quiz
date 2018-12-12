49d11e08bc88a646b63760f76550b588f3de93ce
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2018-12-12 17:34:36 +0300

    Configurates project for connection to backend and transfering data. Adds to project Axios API and connection in to Firebase server

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './QuizList.css';
 import {NavLink} from "react-router-dom";
+import axios from 'axios';
 
 export default class QuizList extends React.Component {
     renderQuizes = () => {
         return [1, 2, 3].map((quiz, index) => {
             return (
-                <div>
-                    <li key={index}>
+                <div key={index}>
+                    <li>
                         <NavLink to={'/quiz/' + quiz}>
                             Тест {quiz}
                         </NavLink>
@@ -16,6 +17,14 @@ export default class QuizList extends React.Component {
             )
         })
     };
+
+    componentDidMount() {
+        axios.get('https://react-quiz-ef355.firebaseio.com/quiz.json').then(
+            response => {
+                console.log(response);
+            }
+        );
+    }
 
     render() {
         return (
