a7d130d60adf7a5ed75cd48607161f6112cab7cd
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2019-02-03 23:12:08 +0300

    Updates

---
+++ precache-manifest.337b6ccd4d4582960023af6c38949c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af45168ecde8330867c4",
+    "url": "/react-quiz/static/js/runtime~main.af45168e.js"
+  },
+  {
+    "revision": "ad880a03f3cba0167f6b",
+    "url": "/react-quiz/static/js/main.ad880a03.chunk.js"
+  },
+  {
+    "revision": "1c2eb4a062d82579381a",
+    "url": "/react-quiz/static/js/1.1c2eb4a0.chunk.js"
+  },
+  {
+    "revision": "ad880a03f3cba0167f6b",
+    "url": "/react-quiz/static/css/main.254be219.chunk.css"
+  },
+  {
+    "revision": "0d14c5ed8c4c7ed39cc66797e258edb6",
+    "url": "/react-quiz/index.html"
+  }
+];
